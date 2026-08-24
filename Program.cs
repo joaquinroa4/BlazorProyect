b0807aa2d522f1df18e404f057563dbe4b9c8454
Joaquin Roa <joaquinroa44@gmail.com>
2026-08-24 02:16:06 -0300

    Registro el DBContext al Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using ProyectoBlazor.Components;
+using Microsoft.EntityFrameworkCore;
+using ProyectoBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,9 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 app.UseHttpsRedirection();
 
