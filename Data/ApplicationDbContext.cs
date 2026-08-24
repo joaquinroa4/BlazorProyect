911892faa6cb8d175162299aaca32e08e12354fc
Joaquin Roa <joaquinroa44@gmail.com>
2026-08-24 02:13:13 -0300

    Añado las relaciones de las tablas

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,6 +16,89 @@ public class ApplicationDbContext : DbContext
     public DbSet<SesionEstudio> SesionesEstudio { get; set; }
     public DbSet<Etiqueta> Etiquetas { get; set; }
     public DbSet<TareaEtiqueta> TareasEtiquetas { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // ==========================================
+        // USUARIO -> MATERIA
+        // ==========================================
+
+        modelBuilder.Entity<Materia>()
+            .HasOne(m => m.Usuario)
+            .WithMany(u => u.Materias)
+            .HasForeignKey(m => m.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        // ==========================================
+        // USUARIO -> TAREA
+        // ==========================================
+
+        modelBuilder.Entity<Tarea>()
+            .HasOne(t => t.Usuario)
+            .WithMany(u => u.Tareas)
+            .HasForeignKey(t => t.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade); // .OnDelete(DeleteBehavior.Cascade)<-- Borra los registros en cascada. Si se borra el registro padre, los registros hijos también se borran
+
+
+        // ==========================================
+        // MATERIA -> TAREA
+        // ==========================================
+
+        modelBuilder.Entity<Tarea>()
+            .HasOne(t => t.Materia)
+            .WithMany(m => m.Tareas)
+            .HasForeignKey(t => t.MateriaId)
+            .OnDelete(DeleteBehavior.Restrict); // .OnDelete(DeleteBehavior.Restrict)<-- No permite borrar el registro padre si tiene registros hijos   
+
+
+        // ==========================================
+        // USUARIO -> SESION DE ESTUDIO
+        // ==========================================
+
+        modelBuilder.Entity<SesionEstudio>()
+            .HasOne(s => s.Usuario)
+            .WithMany(u => u.SesionesEstudio)
+            .HasForeignKey(s => s.UsuarioId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        // ==========================================
+        // TAREA -> SESION DE ESTUDIO
+        // ==========================================
+
+        modelBuilder.Entity<SesionEstudio>()
+            .HasOne(s => s.Tarea)
+            .WithMany(t => t.SesionesEstudio)
+            .HasForeignKey(s => s.TareaId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        // ==========================================
+        // TAREA <-> ETIQUETA
+        // RELACION MUCHOS A MUCHOS
+        // ==========================================
+
+        modelBuilder.Entity<TareaEtiqueta>()
+            .HasKey(te => new
+            {
+                te.TareaId,
+                te.EtiquetaId
+            });
+
+        modelBuilder.Entity<TareaEtiqueta>()
+            .HasOne(te => te.Tarea)
+            .WithMany(t => t.TareasEtiquetas)
+            .HasForeignKey(te => te.TareaId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<TareaEtiqueta>()
+            .HasOne(te => te.Etiqueta)
+            .WithMany(e => e.TareasEtiquetas)
+            .HasForeignKey(te => te.EtiquetaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
 
 //El EF traduce esto como las tablas de mi base de datos
