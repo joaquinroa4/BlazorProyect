6eb560042392856c723acdefa6b59b8474cf1e8c
Joaquin Roa <joaquinroa44@gmail.com>
2026-09-01 22:39:40 -0300

    Añado servicio de UsuarioActual

--- Program.cs
+++ Program.cs
@@ -2,21 +2,27 @@ using ProyectoBlazor.Components;
 using Microsoft.EntityFrameworkCore;
 using ProyectoBlazor.Data;
 using ProyectoBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddBlazoredLocalStorage();
     
 //EntityFramework Core configuration to use SQL Server with the connection string from appsettings.json
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register the UsuarioService for dependency injection
+// Register the Services for dependency injection
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<MateriaService>();
+builder.Services.AddScoped<UsuarioActualService>();
+
+
 
 var app = builder.Build();
 
